fcccee29c6520904c7cd91531c27f052b2b25ed2
JonatanDahl <jdahl.junior@gmail.com>
2026-05-12 08:23:02 +0200

    Enhance BookRequestDto validation with additional constraints for title, ISBN, and publication year

--- src/main/java/com/jonatan/libraryapi/dto/book/BookRequestDto.java
+++ src/main/java/com/jonatan/libraryapi/dto/book/BookRequestDto.java
@@ -1,19 +1,26 @@
 package com.jonatan.libraryapi.dto.book;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public class BookRequestDto {
 
         @NotBlank(message = "Title is required")
+        @Size(min= 2, max = 100, message = "Title must be less than 100 characters")
         private String title;
 
         @NotNull(message = "Author ID is required")
         private Long authorId;
 
+        @NotBlank(message = "ISBN is required")
+        @Size(min = 10, max = 20, message = "ISBN must be between 10 and 20 characters")
         private String isbn;
 
-        private int publicationYear;
+        @NotNull(message = "Publication year is required")
+        @Min(value = 1450, message = "Publication year must be after 1450")
+        private Integer publicationYear;
 
         public BookRequestDto() {
         }
